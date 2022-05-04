904c4bc6f2626b5b48f0a8bf7eb056094a8806aa
Aloys Aboge Jr <76847396+AbogeJr@users.noreply.github.com>
2022-05-04 17:35:33 +0300

    styling entry pages

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+
+#User creation Form mod
 # Create your views here.
 
 def register(request):
